6fed861a9a5a1355cb09bb6fbd0a89b9fbfb2486
Kunzite1 <wang_zhandong@outlook.com>
2026-09-08 15:58:50 +0800

    feat: 配置OLED的I2C接口

--- stm32proj/Core/Src/main.c
+++ stm32proj/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "dac.h"
 #include "dma.h"
+#include "i2c.h"
 #include "tim.h"
 #include "gpio.h"
 
@@ -92,6 +93,7 @@ int main(void)
   MX_DMA_Init();
   MX_DAC_Init();
   MX_TIM6_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
